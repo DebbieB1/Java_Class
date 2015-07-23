28ae2c65b5fdde425dff36d6aec310490ee399eb
Student <Student@Student8-PC>
2015-07-23 14:08:09 -0500

    again

---
+++ CourseProject/src/com/improving/monopoly/GoSquare.java
@@ -0,0 +1,12 @@
+package com.improving.monopoly;
+
+public class GoSquare extends Square {
+	public GoSquare() {
+		super("Go");
+	}
+	
+	@Override
+	public void landOnBy(Player player) {
+		player.credit(200);
+	}
+}
