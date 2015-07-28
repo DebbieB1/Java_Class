272f272fd3ef8d70351e329aaf3c39d37e948124
DebbieB1 <Debbie.Benge@ttiinc.com>
2015-07-28 11:33:49 -0500

    oops

--- CourseProject/src/com/improving/monopoly/LuxTaxSquare.java
+++ CourseProject/src/com/improving/monopoly/LuxTaxSquare.java
@@ -1,12 +1,13 @@
 package com.improving.monopoly;
 
 public class LuxTaxSquare extends Square {
-	public LuxTaxSquare() {
-		super("Luxury Tax");
-	}
-	
-	@Override
-	public void landOnBy(Player player) {
-		player.debit(75);
-	}
+    public LuxTaxSquare() {
+        super("Luxury Tax");
+    }
+
+    @Override
+    public void landOnBy(Player player) {
+        player.debit(75);
+        
+    }
 }
